e77177f64bb70e7305855835b731942a1131484e
Jelena Vasić <jelena.vasic@tudublin.ie>
2026-02-26 16:48:35 +0000

    configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
 // Replace the placeholder values below with your Firebase project configuration.
 // Find these in the Firebase console:
 //   Project Settings > General > Your apps > SDK setup and configuration
-var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyDOi0eDNZsRezN1SRInY7j0KWdAlLSoZLU",
+  authDomain: "vreme-26771.firebaseapp.com",
+  databaseURL: "https://vreme-26771-default-rtdb.firebaseio.com",
+  projectId: "vreme-26771",
+  storageBucket: "vreme-26771.firebasestorage.app",
+  messagingSenderId: "549563830151",
+  appId: "1:549563830151:web:ca8b3a2e89368724518d22",
+  measurementId: "G-R1E8E25S83"
 };
+
