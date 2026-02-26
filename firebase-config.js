b010e70c5bf65ab964ed2113d4354225fef9f9a6
Jelena Vasić <jelena.vasic@tudublin.ie>
2026-02-26 21:27:27 +0000

    complete

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDOi0eDNZsRezN1SRInY7j0KWdAlLSoZLU",
   authDomain: "vreme-26771.firebaseapp.com",
-  databaseURL: "https://vreme-26771-default-rtdb.firebaseio.com",
   projectId: "vreme-26771",
   storageBucket: "vreme-26771.firebasestorage.app",
   messagingSenderId: "549563830151",
   appId: "1:549563830151:web:ca8b3a2e89368724518d22",
   measurementId: "G-R1E8E25S83"
 };
-
